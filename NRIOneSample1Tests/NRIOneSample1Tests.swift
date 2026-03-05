8395a2bffacb01492841f093adf95ae5f9f52b95
akshayadav <akshayadav@gmail.com>
2026-03-05 14:39:48 -0800

    Initial Commit

---
+++ NRIOneSample1Tests/NRIOneSample1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  NRIOneSample1Tests.swift
+//  NRIOneSample1Tests
+//
+//  Created by Akshay Yadav on 3/5/26.
+//
+
+import Testing
+@testable import NRIOneSample1
+
+struct NRIOneSample1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
